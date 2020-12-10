4b16f52e6353c51926d3f3730323d567cb28c5c5
Vedat Bozkurt <info@wedat.org>
2020-12-10 19:52:16 +0300

    main sms class update

--- src/Sms.php
+++ src/Sms.php
@@ -4,24 +4,43 @@
  * @Email: info@wedat.org
  * @Date: 2020-12-10 18:52:47
  * @LastEditors: @vedatbozkurt
- * @LastEditTime: 2020-12-10 19:43:17
+ * @LastEditTime: 2020-12-10 19:51:28
  */
 
 namespace Vedatbozkurt\Sms;
 
 class Sms
 {
-    private function setGateway()
+    public $sms_driver;
+
+    public function __construct()
     {
         $driver = config('sms.driver');
         $class = 'Vedatbozkurt\Sms\Gateway\\' . $driver;
-        $sms_driver = new $class;
-        return $sms_driver;
+        $this->sms_driver = new $class;
     }
+
     public function getPackage()
     {
-        $sms_driver = $this->setGateway();
-        $data = $sms_driver->getPackage();
+        $data = $this->sms_driver->getPackage();
+        return $data;
+    }
+
+    public function getHeaders()
+    {
+        $data = $this->sms_driver->getHeader();
+        return $data;
+    }
+
+    public function getBalance()
+    {
+        $data = $this->sms_driver->getBalance();
+        return $data;
+    }
+
+    public function sendSms($phoneNumbers, $message)
+    {
+        $data = $this->sms_driver->sendSms($phoneNumbers, $message);
         return $data;
     }
 }
