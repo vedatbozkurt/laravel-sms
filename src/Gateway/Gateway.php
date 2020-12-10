523b417ae82e54d914fb8d7dfaf0356df10d64e5
Vedat Bozkurt <info@wedat.org>
2020-12-10 19:13:38 +0300

    gateway methods

--- src/Gateway/Gateway.php
+++ src/Gateway/Gateway.php
@@ -4,12 +4,15 @@
  * @Email: info@wedat.org
  * @Date: 2020-12-10 19:02:31
  * @LastEditors: @vedatbozkurt
- * @LastEditTime: 2020-12-10 19:03:12
+ * @LastEditTime: 2020-12-10 19:13:05
  */
 
 namespace Vedatbozkurt\Sms;
 
 interface Gateway
 {
-    // Build your next great package.
+    public function sendSms($phoneNumbers, $message);
+    public function getBalance();
+    public function getHeader();
+    public function getPackage();
 }
