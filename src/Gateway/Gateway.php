eb7972b424b06fe9e886e0267a128ce32533acab
Vedat Bozkurt <info@wedat.org>
2020-12-10 19:03:45 +0300

    gateway created for sms providers

---
+++ src/Gateway/Gateway.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * @Author: @vedatbozkurt
+ * @Email: info@wedat.org
+ * @Date: 2020-12-10 19:02:31
+ * @LastEditors: @vedatbozkurt
+ * @LastEditTime: 2020-12-10 19:03:12
+ */
+
+namespace Vedatbozkurt\Sms;
+
+interface Gateway
+{
+    // Build your next great package.
+}
